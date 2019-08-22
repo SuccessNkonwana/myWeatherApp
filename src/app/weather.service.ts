0131724907cb7f7165b842b0e42527cfaa06cab1
success <nkonwanasuccess@gmail.com>
2019-08-22 08:43:32 +0200

    second commit

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,8 +9,8 @@ export class WeatherService {
   constructor(private httpClient: HttpClient) {}
 
   getWeatherFromApi(city: string){
-    //return this.httpClient.get(`https://api.apixu.com/v1/current.json?key=9720ed03e296405e814134515191507&q=${city}`);
-    // return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=a6cef6b4ba5fc693969548b9f10b4650');
-    return this.httpClient.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&APPID=a6cef6b4ba5fc693969548b9f10b4650');
+    
+    return this.httpClient.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&APPID=your appid');
   }
+  
 }
